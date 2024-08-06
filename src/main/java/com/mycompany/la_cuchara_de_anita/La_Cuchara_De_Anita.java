3c7595a3dcc0eb2e0604baa78a5a185b0d9d6c5d
Axel <Axel@ASUS-VIVOBOOK>
2024-08-05 18:14:24 -0600

    Project_Base

---
+++ src/main/java/com/mycompany/la_cuchara_de_anita/La_Cuchara_De_Anita.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.la_cuchara_de_anita;
+
+/**
+ *
+ * @author Axel
+ */
+public class La_Cuchara_De_Anita {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
